a1dcb982132fec33b2330cb4edf4369093b011af
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-05-03 12:50:41 +0530

    Sliding complete

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import "./App.css";
+// import Help from "./components/Help";
+import Sliding from "./components/Sliding";
+
+
+function App() {
+  return (
+    <div className="App">
+<Sliding/>
+{/* <Help/> */}
+    </div>
+  );
+}
+
+export default App;
