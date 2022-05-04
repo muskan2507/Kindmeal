41599d50228e4d3ac101b8e942b5a7699b8e39ae
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-05-04 20:24:05 +0530

    Directory Complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AddDirectory from "./components/AddDirectory";
+import Article from "./components/Article";
+import Directory from "./components/Directory";
 import Help from "./components/Help";
 import Sliding from "./components/Sliding";
 
 
 function App() {
+ 
+
   return (
     <div className="App">
-<Sliding/>
 
+<Routes>
+  <Route path="/addshop" element={<AddDirectory/>}/>
+  <Route path="/" element={<Directory/>}/>
+
+</Routes>
+
+
+{/* <Sliding/> */}
+
+{/* <AddDirectory/> */}
+{/* <Directory/> */}
 {/* <Help/> */}
+
+
 
     </div>
   );
