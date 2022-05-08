dfee75a3c078d0e9d2288e6f17be2feabada63d0
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-05-08 14:24:41 +0530

    Update MealDeals.jsx

--- src/Pages/MealDeals.jsx
+++ src/Pages/MealDeals.jsx
@@ -67,32 +67,6 @@ const handleLocationChange=()=>{
 
   return (
     <div>
-    
-      <div>
-        <div
-          style={{
-            backgroundColor: "#ffffff",
-            width: "1060px",
-            height: "270px",
-            margin: "auto",
-            marginTop: "0px",
-            marginBottom: "30px",
-            padding: "0px 0px 0px 0px",
-            border: "1px solid #dddddd",
-            color: "#333333",
-            boxShadow: "0px 0px 6px #e0e0e0",
-            fontSize: "14px",
-          }}
-        >
-          <a href="https://www.kindmeal.my/advertising.htm" target="_blank">
-            <img
-              src="https://www.kindmeal.my/images/ads/banner_advertising-1100x280.png"
-              style={{ width: "100%" }}
-              border="0"
-            />
-          </a>
-        </div>
-      </div>
 
       <div>
         
@@ -212,7 +186,7 @@ filteredList.map((item)=>(
 
     </div>
 
-    
+
 
       <div
         style={{
